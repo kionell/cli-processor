b6a3857a6cbffaca4e641329b9123ab58bbab64e
Kionell <sashakostyunin1@gmail.com>
2022-01-23 20:24:45 +0200

    Replace old double quotes regex with more advanced

--- src/Parsers/CommandParser.ts
+++ src/Parsers/CommandParser.ts
@@ -103,11 +103,37 @@ export class CommandParser {
 
     /**
      * Combine double quotes into one arg and split all args by space.
+     * Let's take a look at the examples:
+     *  1) Double quotes will not be included if they doesn't have a pair or backslash.   
+     *     String: "123  \"
+     *     Result: ['123', '\"']
+     * 
+     *  2) Any backslashed double quote will became a part of an argument or create it's own argument.
+     *     String: \"123  \"
+     *     Result: ['\"123', '\"']
+     * 
+     *  3) Backslashed double quotes will not form a pair.
+     *     String: \"123  \"   "
+     *     Result: ['\"123', '\"']
+     * 
+     *  4) Backslashed double quotes also will be ignored while being in actual double quotes pair.
+     *     String: "123  \"   "
+     *     Result: ['"123  \"   "']
+     * 
+     *  5) Empty double quotes will return nothing.
+     *     String: ""
+     *     Result: []
+     * 
+     *  6) Allows any characters before double quotes until previous one is backslash.
+     *     String: \\\\"  \\g\"  f\"  gf\"fdg  g"gfgdfg
+     *     Result: ['\\\\"', '\\g\"', 'f\"', 'gf\"', 'fdg', 'g', 'gfgdfg']
      */
-    let args = input.match(/[^"\s]+|"(?:\\"|[^"])*"/gm) ?? [];
+    let args = input.match(/[^\s]+(?:\\)+"|(?:\\)"[^"\s]*|[^"\s]+|"(?:\\"|[^"])*(?:[^\\"]")/gm) ?? [];
 
     /**
-     * Remove double quotes from the args except backslash double quotes.
+     * Remove double quotes and backslashes.
+     * If there is no backslash before the double quotes, remove the quotes themselves.
+     * If there is a backslash before double quotes, remove only the backslash.
      */
     args = args.map((arg) => arg.replace(/(?<!\\)"/gm, '').replace(/\\/g, ''));
 
