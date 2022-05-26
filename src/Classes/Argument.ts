bb210de5b6c65b3223624067b7de93f3771ea675
kionell <sashakostyunin1@gmail.com>
2022-05-26 04:28:21 +0300

    Add argument typing system

--- src/Classes/Argument.ts
+++ src/Classes/Argument.ts
@@ -1,65 +1,142 @@
+import { ArgumentType } from '../Types';
 import { IArgument } from '../Interfaces';
 
 /**
  * An argument.
  */
-export class Argument implements IArgument {
+export class Argument<T extends string | number | boolean = string | number | boolean> implements IArgument<T> {
   /**
    * The argument description.
    */
-  description = '';
-
-  /**
-   * Split words of argument value.
-   */
-  values: any[] = [];
+  readonly description = '';
 
   /**
    * Whether the argument is required or not.
    */
-  isRequired = false;
+  readonly isRequired = false;
 
   /** 
    * The number of words needed to be considered as this argument.
    */
-  minLength = 0;
+  readonly minLength = 0;
 
   /** 
    * The max number of words possible to be considered as this argument.
    */
-  maxLength = 0;
+  readonly maxLength;
+
+  /**
+   * Default value of this argument.
+   */
+  readonly defaultValue: T = '' as T;
+
+  /**
+   * The type of this argument at runtime.
+   */
+  readonly type: ArgumentType = 'string';
+
+  /**
+   * Current value of this argument.
+   */
+  private _value: T | null = null;
+
+  /**
+   * Raw argument values.
+   */
+  private _raw: string[] = [];
 
   /**
    * Creates a new instance of an argument.
    * @param params The agrument params.
    * @constructor
    */
-  constructor(params: Partial<IArgument> = {}) {
+  constructor(params: Partial<IArgument<T>> = {}) {
     Object.assign(this, params);
 
-    this.maxLength = Math.max(this.minLength, this.maxLength);
-  }
-
-  /**
-   * Stringified value of the agrument.
-   */
-  get value(): string {
-    return this.values.join(' ');
-  }
-
-  set value(val: string) {
-    this.values = val?.split(' ') ?? this.values;
+    this.maxLength = Math.max(this.minLength, params?.maxLength ?? 0);
   }
 
   /**
    * Actual length of the argument.
    */
   get length(): number {
-    return this.values.length;
+    return this._raw.length;
   }
 
-  set length(val: number) {
-    this.values.length = Math.max(0, Math.min(this.values.length, val));
+  /**
+   * @returns Raw input split into separate words.
+   */
+  *keys(): Generator<string> {
+    for (const v of this._raw) yield v;
+  }
+
+  /**
+   * @returns The default value of this argument converted to type of this argument.
+   */
+  getDefaultValue(): T {
+    return this._castValue(this.defaultValue);
+  }
+
+  /**
+   * @returns Current value of this argument converted to type of this argument.
+   */
+  getValue(): T | null {
+    return this._value && this._castValue(this._value);
+  }
+
+  /**
+   * @returns Current value of this argument or default value.
+   */
+  getValueOrDefault(): T {
+    return this.getValue() ?? this.getDefaultValue();
+  }
+
+  /**
+   * Sets the value of this argument.
+   * @param value Input value.
+   */
+  setValue(value: T | null): void {
+    if (value === null) {
+      this._raw.length = 0;
+      this._value = value;
+
+      return;
+    }
+
+    const isString = typeof value === 'string';
+    const isNumber = typeof value === 'number';
+    const isBoolean = typeof value === 'boolean';
+
+    if (!isString && !isNumber && !isBoolean) return;
+
+    const stringified = value.toString();
+    const values = stringified.split(' ');
+
+    if (values.length < this.minLength) return;
+    if (values.length > this.maxLength) return;
+
+    this._raw = values;
+    this._value = value as T;
+  }
+
+  /**
+   * Converts a value to the type of this argument.
+   * @param value Input value.
+   * @returns Cast value.
+   */
+  private _castValue(value: unknown): T {
+    switch (this.type) {
+      case 'boolean':
+        return Boolean(value) as T;
+
+      case 'integer':
+        return (parseInt(value as string) || Number()) as T;
+
+      case 'float':
+        return (parseFloat(value as string) || Number()) as T;
+    }
+
+    return (value ? String(value) : String()) as T;
   }
 
   /**
@@ -70,37 +147,27 @@ export class Argument implements IArgument {
       return input.split(' ').length > 1 ? `"${input}"` : input;
     };
 
-    const values = this.values.map((value) => {
-      if (typeof value === 'number') {
-        return value.toString();
-      }
-
-      return typeof value === 'string' ? value : '';
-    });
-
-    return values
-      .filter((v) => v)
-      .map((v) => addDoubleQuotes(v))
-      .join(' ');
+    return this._raw.map((v) => addDoubleQuotes(v)).join(' ');
   }
 
-  clone(): Argument {
-    return new Argument({
+  clone(): this {
+    const TypedArgument = this.constructor as new (params: Partial<IArgument<T>>) => this;
+
+    const argument = new TypedArgument({
       description: this.description,
-      values: this.values.slice(),
       isRequired: this.isRequired,
       minLength: this.minLength,
       maxLength: this.maxLength,
+      defaultValue: this.defaultValue,
+      type: this.type,
     });
+
+    argument.setValue(this.getValue());
+
+    return argument;
   }
 
-  equals(other: Argument): boolean {
-    if (this.length !== other.length) return false;
-
-    for (const value of this.values) {
-      if (!other.values.includes(value)) return false;
-    }
-
-    return true;
+  equals(other: IArgument<T>): boolean {
+    return this.getValue() === other.getValue();
   }
 }
--- src/Interfaces/IArgument.ts
+++ src/Interfaces/IArgument.ts
@@ -1,40 +1,70 @@
-import {
-  ICloneable,
-  IStringable,
-  IComparable,
-} from './Types';
+import { ArgumentType } from '../Types';
+import { ICloneable, IComparable } from './Types';
 
 /**
  * An argument.
  */
-export interface IArgument extends ICloneable, IStringable, IComparable {
+export interface IArgument<
+  T extends string | number | boolean = string | number | boolean
+> extends ICloneable, IComparable {
   /**
    * The argument description.
    */
-  description: string;
-
-  /**
-   * Stringified argument value.
-   */
-  value: string;
-
-  /**
-   * Split words of argument value. 
-   */
-  values: string[];
+  readonly description: string;
 
   /**
    * Whether the argument is required or not.
    */
-  isRequired: boolean;
+  readonly isRequired: boolean;
 
   /** 
    * The number of words needed to be considered as this argument.
    */
-  minLength: number;
+  readonly minLength: number;
 
   /** 
    * The max number of words possible to be considered as this argument.
    */
-  maxLength: number;
+  readonly maxLength: number;
+
+  /**
+   * Default value of this argument.
+   */
+  readonly defaultValue: T;
+
+  /**
+   * The type of this argument at runtime.
+   */
+  readonly type: ArgumentType;
+
+  /**
+   * Actual length of the argument.
+   */
+  readonly length: number;
+
+  /**
+   * @returns Raw input split into separate words.
+   */
+  keys(): Generator<string>
+
+  /**
+   * @returns The default value of this argument converted to type of this argument.
+   */
+  getDefaultValue(): T;
+
+  /**
+   * @returns Current value of this argument converted to type of this argument.
+   */
+  getValue(): T | null;
+
+  /**
+   * @returns Current value of this argument or default value.
+   */
+  getValueOrDefault(): T;
+
+  /**
+   * Sets the value of this argument.
+   * @param value Input value.
+   */
+  setValue(value: T | null): void;
 }
