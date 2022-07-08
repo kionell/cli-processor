93a407fd87106d0ed35c7a2db1ca4fb483b270a4
kionell <sashakostyunin1@gmail.com>
2022-07-08 14:47:11 +0300

    Add command methods to work with options & return subcommands map

--- src/Classes/Command.ts
+++ src/Classes/Command.ts
@@ -1,9 +1,5 @@
-import {
-  ICommand,
-  IHasArgument,
-  IHasFlags,
-  IHasSubcommands,
-} from '../Interfaces';
+import { ICommand, IOption } from '../Interfaces';
+import { OptionType } from '../Types';
 
 /**
  * A command.
@@ -30,6 +26,26 @@ export class Command implements ICommand {
   description = '';
 
   /**
+   * Shortened description of this command.
+   */
+  shortDescription = '';
+
+  /**
+   * Example usage of this command.
+   */
+  examples: string[] = [];
+
+  /**
+   * The list of command options.
+   */
+  options: IOption[] = [];
+
+  /**
+   * Subcommands of this command.
+   */
+  subcommands: Map<string, ICommand> = new Map();
+
+  /**
    * The command execute function.
    */
   execute(...args: any[]): any {
@@ -46,6 +62,97 @@ export class Command implements ICommand {
   }
 
   /**
+   * Adds a new option to this command.
+   * @param option Option instance.
+   */
+  addOption(option: IOption): void {
+    this.options.push(option);
+  }
+
+  /**
+   * Adds a new subcommand to this command.
+   * @param subcommand Subcommand instance.
+   */
+  addSubcommand(subcommand: ICommand): void {
+    this.subcommands.set(subcommand.name, subcommand);
+  }
+
+  /**
+   * Returns if array of options has any option of specific type.
+   * @param type Target option type.
+   */
+  hasOptionOfType(type: OptionType): boolean {
+    if (!this.options.length) return false;
+
+    for (const option of this.options) {
+      if (option.type === type) return true;
+    }
+
+    return false;
+  }
+
+  /**
+   * Returns all options of specific type.
+   * @param type Target option type.
+   * @returns Filtered option array.
+   */
+  getOptionsOfType(type: OptionType): IOption[] {
+    return this.options.filter((o) => o.type === type);
+  }
+
+  /**
+   * Searches for an actual option instance by option name.
+   * @param name Option name.
+   * @returns Option instance or null if not found.
+   */
+  getOptionByName(name: string): IOption | null {
+    if (!this.options.length) return null;
+
+    for (const option of this.options) {
+      if (option.name === name) return option;
+    }
+
+    return null;
+  }
+
+  /**
+   * Searches for an actual option instance by a class.
+   * @param Option Option class.
+   * @returns Option instance or null if not found.
+   */
+  getOption<T extends IOption>(Option: new () => T): T | null {
+    if (!this.options.length) return null;
+
+    for (const option of this.options) {
+      if (option instanceof Option) return option;
+    }
+
+    return null;
+  }
+
+  /**
+   * Tries to get current value of the specified option.
+   * @param Option Option class.
+   * @returns Current value or null.
+   */
+  getValue<T extends IOption>(Option: new () => T): ReturnType<T['getValue']> {
+    const value = this.getOption(Option)?.getValue() ?? null;
+
+    return value as ReturnType<T['getValue']>;
+  }
+
+  /**
+   * Gets current or default value of the specified option.
+   * @param Option Option class.
+   * @returns Current or default value of the option.
+   */
+  getValueOrDefault<T extends IOption>(Option: new () => T): ReturnType<T['getValueOrDefault']> {
+    const value = this.getOption(Option)?.getValueOrDefault() ?? String();
+
+    return value as ReturnType<T['getValueOrDefault']>;
+  }
+
+  /**
    * A string representation of the argument.
    */
   toString(): string {
@@ -55,39 +162,24 @@ export class Command implements ICommand {
   clone(): this {
     const TypedCommand = this.constructor as new (params: Partial<ICommand>) => this;
 
-    const result = new TypedCommand({
+    const cloned = new TypedCommand({
       name: this.name,
+      aliases: this.aliases,
       title: this.title,
-      aliases: this.aliases.slice(),
       description: this.description,
+      shortDescription: this.shortDescription,
+      examples: this.examples,
+      options: this.options.map((o) => o.clone()),
       execute: this.execute,
     });
 
-    // eslint-disable-next-line @typescript-eslint/no-this-alias
-    const original = this as this & IHasFlags & IHasArgument & IHasSubcommands;
-    const cloned = result as this & IHasFlags & IHasArgument & IHasSubcommands;
+    cloned.subcommands = new Map();
 
-    if (original.flags) {
-      cloned.flags = new Map();
-
-      for (const flag of original.flags.values()) {
-        cloned.flags.set(flag.name, flag.clone());
-      }
+    for (const subcommand of this.subcommands.values()) {
+      cloned.subcommands.set(subcommand.name, subcommand.clone());
     }
 
-    if (original.subcommands) {
-      cloned.subcommands = new Map();
-
-      for (const subcommand of original.subcommands.values()) {
-        cloned.subcommands.set(subcommand.name, subcommand.clone());
-      }
-    }
-
-    if (original.arg) {
-      cloned.arg = original.arg?.clone();
-    }
-
-    return result;
+    return cloned;
   }
 
   equals(other: Command): boolean {
--- src/Interfaces/ICommand.ts
+++ src/Interfaces/ICommand.ts
@@ -1,4 +1,6 @@
+import { IOption } from './IOption';
 import { ICloneable, IComparable } from './Types';
+import { OptionType } from '../Types';
 
 /**
  * A command.
@@ -10,19 +12,90 @@ export interface ICommand extends ICloneable, IComparable {
   name: string;
 
   /**
-   * The command title.
-   */
-  title: string;
-
-  /**
    * The command aliases.
    */
   aliases: string[];
 
   /**
+   * The command title.
+   */
+  title: string;
+
+  /**
    * The command description.
    */
   description: string;
+
+  /**
+   * Shortened description of this command.
+   */
+  shortDescription: string;
+
+  /**
+   * Example usage of this command.
+   */
+  examples: string[];
+
+  /**
+   * The list of command options.
+   */
+  options: IOption[];
+
+  /**
+   * Subcommands of this command.
+   */
+  subcommands: Map<string, ICommand>;
+
+  /**
+   * Adds a new option to this command.
+   * @param option Option instance.
+   */
+  addOption(option: IOption): void;
+  /**
+   * Adds a new subcommand to this command.
+   * @param subcommand Subcommand instance.
+   */
+  addSubcommand(subcommand: ICommand): void;
+  /**
+   * Returns if array of options has any option of specific type.
+   * @param type Target option type.
+   */
+  hasOptionOfType(type: OptionType): boolean;
+
+  /**
+   * Returns all options of specific type.
+   * @param type Target option type.
+   * @returns Filtered option array.
+   */
+  getOptionsOfType(type: OptionType): IOption[];
+
+  /**
+   * Searches for an actual option instance by option name.
+   * @param name Option name.
+   * @returns Option instance or null if not found.
+   */
+  getOptionByName(name: string): IOption | null;
+
+  /**
+   * Searches for an actual option instance by a class.
+   * @param Option Option class.
+   * @returns Option instance or null if not found.
+   */
+  getOption<T extends IOption>(Option: new () => T): T | null;
+
+  /**
+   * Tries to get current value of the specified option.
+   * @param Option Option class.
+   * @returns Current value or null.
+   */
+  getValue<T extends IOption>(Option: new () => T): ReturnType<T['getValue']>;
+
+  /**
+   * Gets current or default value of the specified option.
+   * @param Option Option class.
+   * @returns Current or default value of the option.
+   */
+  getValueOrDefault<T extends IOption>(Option: new () => T): ReturnType<T['getValueOrDefault']>;
 
   /**
    * The command execute function.
